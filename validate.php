2efa54e47a875dec5d2f5a7174057c129aa73284
Utkapatel-algoma <Bittupatel3312@gmail.com>
2025-05-23 21:29:41 +0000

    Added login to confirm authentication.
    As per my understanding - professor already helped to check username and password. Added login to confirm that the user is authenticated or user can be re-directed to login.php.

--- validate.php
+++ validate.php
@@ -9,19 +9,22 @@
   $_SESSION['username'] = $username;
   $password = $_REQUEST['password'];
 
+//checking if credentials provided are coorect or not
   if ($valid_username == $username && $valid_password == $password) {
     $_SESSION['authenticated'] = 1;
-    header('Location: /');
+    header('Location: /'); //Pushed user to index.php after successful login
+    exit(); // stopped script after this step.
   } else {
 
     if (!isset($_SESSION['failed_attempts'])){
       $_SESSION['failed_attempts'] = 1;
     } else {
-      $_SESSION['failed_attempts'] = $_SESSION['failed_attempts'] + 1;
+      $_SESSION['failed_attempts'] += 1;
     }
 
-    //header... redirect to login.php
-    echo "This is unsuccessful attempt number" . $_SESSION['failed_attempts'];
+    //Re-directed to login page now
+    header('Location: /login.php');
+    exit();// stopped script after this step.
   
   }
 
